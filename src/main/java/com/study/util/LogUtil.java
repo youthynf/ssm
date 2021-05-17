6b28a25127dc745fd28187b69866bbe6b6957657
yannengfa <940436475@qq.com>
2021-05-17 10:51:19 +0800

    aop获取方法参数

--- src/main/java/com/study/util/LogUtil.java
+++ src/main/java/com/study/util/LogUtil.java
@@ -1,16 +1,24 @@
 package com.study.util;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 @Component
 @Aspect
 public class LogUtil {
     @Before("execution( public int com.study.util.Calculator.*(int,int))")
-    public static void start(){
-//        System.out.println("XXX方法开始执行，使用的参数是："+ Arrays.asList(objects));
-//        System.out.println(method.getName()+"方法开始执行，参数是："+ Arrays.asList(objects));
-        System.out.println("方法开始执行，参数是：");
+    public static void start(JoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        String name = joinPoint.getSignature().getName();
+        long millis = System.currentTimeMillis();
+        Date date = new Date(millis);
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+        System.out.println(format + " | 方法开始执行，参数是：" + Arrays.asList(args) + ", 名称为：" + name);
     }
 
     @AfterReturning("execution( public int com.study.util.Calculator.*(int,int))")
